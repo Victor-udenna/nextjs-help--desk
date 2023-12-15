48286c6110a0733d0b7e76a4d7502a38be59d8c6
Victor udenna <victorudenna@gmail.com>
2023-12-15 20:56:00 +0100

    tickets

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import TicketList from './TicketList'
 
 const Tickets = () => {
-  return <div>Tickets</div>
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently Open Tickets</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList />
+    </main>
+  )
 }
 
 export default Tickets
